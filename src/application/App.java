12cb0fe659c1c02ff9282114ffa6ff110eaf9ea4
earl-stephens <stephensearl@hotmail.com>
2022-01-28 21:58:00 -0700

    Create a simple window

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,21 @@
 package application;
 
+import javax.swing.JFrame;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Java has 2 main GUI APIs - Swing, and JavaFX
+		// swing is built into Java
+		//JFrame is a class that represents a window on the screen
+		JFrame mainFrame = new JFrame();
+		mainFrame.setSize(600, 400);
+		//tells program to quit when the window is closed.
+		//closes the event loop for the window
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
 
+		
 	}
 
 }
